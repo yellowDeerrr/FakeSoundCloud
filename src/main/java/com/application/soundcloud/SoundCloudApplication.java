8ee6f03e4c17ee080ad59c8c0c197698d168e142
yellowDeerrr <emailforgithubbanksystemsql@gmail.com>
2023-07-09 13:23:46 +0300

    first commit

---
+++ src/main/java/com/application/soundcloud/SoundCloudApplication.java
@@ -0,0 +1,13 @@
+package com.application.soundcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoundCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/application/soundcloud/SoundCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.soundcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
